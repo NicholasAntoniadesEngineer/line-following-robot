5470c1da90576168a5e862ffb9a68c12e1c39696
Nicholas Antoniades <31705980+NicholasAntoniadesEngineer@users.noreply.github.com>
2025-01-04 23:10:50 +0200

    Update stm32_lib.c

--- code/stm32_lib.c
+++ code/stm32_lib.c
@@ -9,25 +9,6 @@
 #include "stm32_lib.h"
 #include "lcd_stm32f0.h"
 
-/* Global Variables ----------------------------------------------------------*/
-static int bcdArray[5];
-
-/* Function Prototypes -------------------------------------------------------*/
-void init_EXTI(void);
-void init_NVIC(void);
-void init_Ports(void);
-void init_ADC(void);
-void init_Tim6(void);
-void init_TIM14(void);
-void init_PWM(void);
-void TIM6_DAC_IRQHandler(void);
-int ADC_POT(int pot, int resolution);
-int ADC_DATA(void);
-void delay_ms(uint32_t counter);
-void EXTI0_1_IRQHandler(void);
-
-/* Functions -----------------------------------------------------------------*/
-
 /**
  * @brief  Initialize External Interrupt
  */
